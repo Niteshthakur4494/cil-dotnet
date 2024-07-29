d7fcd72a224117139aec2cac670e85f976df62b3
Nitesh Thakur <Nitesh.Thakur@philips.com>
2024-07-29 13:36:40 +0530

    Simple console app with Hello World!

---
+++ cli-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cli_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
